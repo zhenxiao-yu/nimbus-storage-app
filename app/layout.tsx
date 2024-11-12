acf03df3cea7672f8b68cb88df7bfba56e8b9f3c
Mark Yu · 于震潇 <zyu347@uwo.ca>
2024-11-11 22:51:17 -0500

    eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from 'next/font/google'
 import "./globals.css";
 
-//font declaration
+// font declaration
 const poppins = Poppins({
     subsets: ['latin'],
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -22,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
    <html lang="en">
-      <body
-        className={`${poppins.variable} font-poppins antialiased`}
-      >
+      <body className={`${poppins.variable} font-poppins antialiased`}>
         {children}
       </body>
     </html>
