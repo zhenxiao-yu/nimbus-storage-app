8c3afdfddb5f471bd2130be6c56af64a4c853951
Mark Yu · 于震潇 <zyu347@uwo.ca>
2024-11-19 19:09:49 -0500

    added auth layout

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from "@/components/AuthForm";
+
+const Login = () => <AuthForm type="login" />;
+
+export default Login;
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from "@/components/AuthForm";
+
+const Register = () => <AuthForm type="register" />;
+
+export default Register;
