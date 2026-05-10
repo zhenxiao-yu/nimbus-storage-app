345f8bee9afb2d5123bbd6a24f9e80245fd1f142
Mark Yu · 于震潇 <zyu347@uwo.ca>
2026-05-10 00:03:45 -0400

    fix: disable conflicting-classname lint for magicui border-beam

--- components/magicui/border-beam.tsx
+++ components/magicui/border-beam.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable tailwindcss/no-contradicting-classname */
 import { cn } from "@/lib/utils";
 
 interface BorderBeamProps {
@@ -35,7 +36,6 @@ export const BorderBeam = ({
         } as React.CSSProperties
       }
       className={cn(
-        // eslint-disable-next-line tailwindcss/no-contradicting-classname
         "pointer-events-none absolute inset-0 rounded-[inherit] [border:calc(var(--border-width)*1px)_solid_transparent] ![mask-clip:padding-box,border-box] ![mask-composite:intersect] [mask:linear-gradient(transparent,transparent),linear-gradient(white,white)] after:absolute after:aspect-square after:w-[calc(var(--size)*1px)] after:animate-border-beam after:[animation-delay:var(--delay)] after:[background:linear-gradient(to_left,var(--color-from),var(--color-to),transparent)] after:[offset-anchor:calc(var(--anchor)*1%)_50%] after:[offset-path:rect(0_auto_auto_0_round_calc(var(--size)*1px))]",
         className,
       )}
